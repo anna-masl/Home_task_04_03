955e828c1b1439fcac3fb710b1ef298a59efa014
anna-masl <anna.maslinnikova@gmail.com>
2025-06-16 17:12:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const userInputYear = prompt('input your year of birth');
+const userInputTown = prompt('input your town');
+const userInputSport = prompt('input your sport details');
+
+debugger
+if (!userInputYear?.trim()){
+    let yearOutput = 'nothing entered'
+    console.log(yearOutput);
+}
+
+else {
+    const positiveYearStr = String(Math.abs(+userInputYear));
+    const userAge = 2025 - Number(userInputYear);
+    if (positiveYearStr.length === 4 && userAge < 150 && userAge > 10 ){
+        console.log(userInputYear);
+        console.log(userAge);
+        yearOutput = userAge;
+    }
+    else { yearOutput = 'not a correct year'
+        console.log(yearOutput);
+    }
+}
+
+if (!userInputTown?.trim()){
+    let townOutput = 'nothing entered'
+    console.log(townOutput);
+}
+
+else {
+    townOutput = userInputTown.trim().toLowerCase();
+
+    switch (townOutput) {
+        case 'kuiv': 
+            alert(`You are ${yearOutput} years old. You live on the capital of Ukraine`);
+        break;
+        case 'vashington':
+            alert(`You are ${yearOutput} years old. You live on the capital of USA`);
+        break;
+        case 'london': 
+            alert(`You are ${yearOutput} years old. You live on the capital of England`);
+        break;
+        default:
+            alert(`You are ${yearOutput} years old. You live in the ${townOutput}`);
+        break;
+    }
+}
